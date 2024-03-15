4b8e2d63faaceeb957bb232a1cc5d9cc9cee6574
Padraig Dooley <padraigdooley@gmail.com>
2024-03-15 21:05:54 +0000

    set upo customstorages

--- mini_shop/settings.py
+++ mini_shop/settings.py
@@ -175,7 +175,7 @@ else:
     AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN")
     STORAGES = {
         "default": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "BACKEND": "custom_storages.CustomS3Boto3Storage",
             "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"},
         }
     }
