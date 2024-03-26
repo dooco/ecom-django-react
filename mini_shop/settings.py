856cadeff9625ca795451be81a1755d0e44db311
Padraig Dooley <padraigdooley@gmail.com>
2024-03-26 18:34:41 +0000

    add digital ocean address to allowed hosts

--- mini_shop/settings.py
+++ mini_shop/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,seashell-app-p2c8o.ondigitalocean.app'"
+).split(",")
 
 
 # Application definition
